acdee1d87e0990c10b5ed5b2aee5d51eb24721aa
Kalpan@MacBook <Kalpan.bhatt@gmail.com>
2017-03-02 22:56:28 -0500

    removed root public folder

---
+++ js/index.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function() {
+	$('.slider').slider({height: 1000, indicators: false});
+});
--- public/js/index.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function() {
-	$('.slider').slider({height: 1000, indicators: false});
-});
