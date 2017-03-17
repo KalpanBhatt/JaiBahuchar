98556451608f18c4362d662e0a81af69cb06053b
Kalpan Bhatt <kalpan.bhatt@gmail.com>
2017-03-17 15:28:31 -0400

    Image Gallery working

--- js/gallery.js
+++ js/gallery.js
@@ -4,7 +4,24 @@ $('#event2').click(function(){
   $('.event2').removeClass('hide-display');
   $('.event2').addClass('active-display');
 });
-
+$('#event3').click(function(){
+  $('.active-display').addClass('hide-display');
+  $('.active-display').removeClass('active-display');
+  $('.event3').removeClass('hide-display');
+  $('.event3').addClass('active-display');
+});
+$('#event4').click(function(){
+  $('.active-display').addClass('hide-display');
+  $('.active-display').removeClass('active-display');
+  $('.event4').removeClass('hide-display');
+  $('.event4').addClass('active-display');
+});
+$('#event1').click(function(){
+  $('.active-display').addClass('hide-display');
+  $('.active-display').removeClass('active-display');
+  $('.event1').removeClass('hide-display');
+  $('.event1').addClass('active-display');
+});
 $('a[data-imagelightbox="f"]').imageLightbox({
       activity: true,
       button: true,
@@ -13,6 +30,7 @@ $('a[data-imagelightbox="f"]').imageLightbox({
       overlay: true,
       selector: 'a[data-imagelightbox="f"]'
     });
+
 $('a[data-imagelightbox="a"]').imageLightbox({
           activity: true,
           button: true,
@@ -20,4 +38,13 @@ $('a[data-imagelightbox="a"]').imageLightbox({
           navigation: true,
           overlay: true,
           selector: 'a[data-imagelightbox="a"]'
+        });
+
+$('a[data-imagelightbox="b"]').imageLightbox({
+          activity: true,
+          button: true,
+          caption: true,
+          navigation: true,
+          overlay: true,
+          selector: 'a[data-imagelightbox="b"]'
         });
